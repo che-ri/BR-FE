cd2217ac2421003d2a39b6179b436a4ff60658e8
che-ri <rnjs1135@gmail.com>
2021-07-18 03:58:06 +0900

    [feat] 라우터 보완

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,15 @@ import Slider from "../components/Slider";
 
 import Inner from "../components/Inner";
 
-import main_banner1 from "../asset/mainBanner/1620351475.png";
-import main_banner2 from "../asset/mainBanner/1620379522.jpg";
-import main_banner3 from "../asset/mainBanner/1630631572.jpg";
-import main_banner4 from "../asset/mainBanner/1638847240.png";
+import main_banner1 from "../asset/mainBnr/1620351475.png";
+import main_banner2 from "../asset/mainBnr/1620379522.jpg";
+import main_banner3 from "../asset/mainBnr/1630631572.jpg";
+import main_banner4 from "../asset/mainBnr/1638847240.png";
 
 const Home = () => {
     return (
         <>
-            <Img src={require("../asset/mainBanner/top_bnr.jpg").default} />
+            <Img src={require("../asset/mainBnr/top_bnr.jpg").default} />
             <Container>
                 <Inner style={{ position: "relative" }}>
                     <Slider
@@ -39,63 +39,63 @@ const Home = () => {
                         show={3}
                         items={[
                             {
-                                url: require("../asset/mainBanner/br_event1.png")
+                                url: require("../asset/mainBnr/br_event1.png")
                                     .default,
                                 title: "해피오더 딜리버리로 간편하게 주문하세요!",
                                 process: "상시진행",
                                 event: "store",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event2.png")
+                                url: require("../asset/mainBnr/br_event2.png")
                                     .default,
                                 title: "배달의 민족에서 빠르게 주문하세요!",
                                 process: "상시진행",
                                 event: "store",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event3.png")
+                                url: require("../asset/mainBnr/br_event3.png")
                                     .default,
                                 title: "요기요에서 손쉽게 주문하세요!",
                                 process: "상시진행",
                                 event: "store",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event4.png")
+                                url: require("../asset/mainBnr/br_event4.png")
                                     .default,
                                 title: "카카오톡 주문하기에서 편리하게 주문하세요!",
                                 process: "상시진행",
                                 event: "store",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event5.jpg")
+                                url: require("../asset/mainBnr/br_event5.jpg")
                                     .default,
                                 title: "KT 멤버십 고객이라면 누구나 파인트 30% 할인!",
                                 process: "",
                                 event: "store",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event6.png")
+                                url: require("../asset/mainBnr/br_event6.png")
                                     .default,
                                 title: "제휴 할인 카드 혜택 안내",
                                 process: "상시진행",
                                 event: "store",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event7.png")
+                                url: require("../asset/mainBnr/br_event7.png")
                                     .default,
                                 title: "2018 한글날 기념 무료 글꼴 공개! 베스킨라빈스체",
                                 process: "상시진행",
                                 event: "online",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event8.png")
+                                url: require("../asset/mainBnr/br_event8.png")
                                     .default,
                                 title: "1회용 컵 사용 줄이기 안내",
                                 process: "상시진행",
                                 event: "store",
                             },
                             {
-                                url: require("../asset/mainBanner/br_event9.jpg")
+                                url: require("../asset/mainBnr/br_event9.jpg")
                                     .default,
                                 title: "2021년 1분기 고객BEST 칭찬점포 안내",
                                 process: "",
@@ -110,8 +110,7 @@ const Home = () => {
                     <Link to="/menu/main">
                         <BRMenu
                             src={
-                                require("../asset/mainBanner/br_menu.png")
-                                    .default
+                                require("../asset/mainBnr/br_menu.png").default
                             }
                             alt="BR MENU"
                         />
@@ -134,7 +133,7 @@ const Home = () => {
                             <Link to="/">
                                 <Store
                                     src={
-                                        require("../asset/mainBanner/img_store.jpg")
+                                        require("../asset/mainBnr/img_store.jpg")
                                             .default
                                     }
                                     alt="BR MENU"
@@ -154,7 +153,7 @@ const Home = () => {
                             <Link to="/">
                                 <Delivery
                                     src={
-                                        require("../asset/mainBanner/img_happyorder_delivery.png")
+                                        require("../asset/mainBnr/img_happyorder_delivery.png")
                                             .default
                                     }
                                     alt="BR MENU"
