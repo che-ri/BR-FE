bb9e9aa7dfcbdc4bf11cb1ce0728aeb0de1a903b
che-ri <rnjs1135@gmail.com>
2021-07-20 23:04:22 +0900

    [feat] 회원가입구현

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -5,52 +5,52 @@ import { Grid } from "./index";
 
 // 인풋 컴포넌트
 /**
- * 
- * @param {*} props 
+ *
+ * @param {*} props
  * - placeholder 인풋 박스에 미리 넣어줄 텍스트
  * - _onChange 인풋에 들어갈 값(text, file 등)이 변경되면 실행할 함수
  * - type 인풋 박스의 타입 (file, text 등)
  * - value 인풋 박스의 값
- * @returns 
+ * @returns
  */
-const Input = (props) => {
-  const {
-    placeholder, _onChange, multiLine,
-    type,} = props;
+const Input = props => {
+    const { placeholder, _onChange, multiLine, type } = props;
 
-  if (multiLine) {
+    if (multiLine) {
+        return (
+            <ElTextarea
+                type={type}
+                rows={10}
+                onChange={_onChange}
+                placeholder={placeholder}
+            />
+        );
+    }
     return (
-        <ElTextarea type={type} rows={10}
-        onChange={_onChange}
-        placeholder={placeholder}
-         />
-    );    
-  }
-  return (
-          <ElInput type={type} onChange={_onChange} placeholder={placeholder}  />
-  );
+        <ElInput type={type} onChange={_onChange} placeholder={placeholder} />
+    );
 };
 
 Input.defaultProps = {
-  multiLine: false,
-  placeholder:"",
-  type: "text",
+    multiLine: false,
+    placeholder: "",
+    type: "text",
+    _onChange: () => {},
 };
 
 const ElTextarea = styled.textarea`
-  border: 1px solid #212121;
-  width: 100%;
-  padding: 12px 4px;
-  box-sizing: border-box;
+    border: 1px solid #212121;
+    width: 100%;
+    padding: 12px 4px;
+    box-sizing: border-box;
 `;
 
 const ElInput = styled.input`
-  border : none;
-  border-bottom: 1px solid #212121;
-  width: 100%;
-  padding: 12px 4px;
-  box-sizing: border-box;
-  
+    border: none;
+    border-bottom: 1px solid #212121;
+    width: 100%;
+    padding: 12px 4px;
+    box-sizing: border-box;
 `;
 
 export default Input;
